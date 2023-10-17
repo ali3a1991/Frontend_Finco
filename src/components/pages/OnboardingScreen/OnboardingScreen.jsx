a65d950e1b3dda08dc5642bfc2fe012a13dad069
mschild91 <michael.schild@mail.de>
2023-10-17 10:43:30 +0200

    added all components and Routes to the frontend

---
+++ src/components/pages/OnboardingScreen/OnboardingScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function OnboardingScreen() {
+  return (
+    <div>
+      <h1>Splash/Onboarding</h1>
+    </div>
+  )
+}
+
+export default OnboardingScreen
---
+++ src/components/pages/OnboardingTutorial/OnboardingTutorial.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function OnboardingTutorial() {
+  return (
+    <div>
+      <h1>Tutorial</h1>
+    </div>
+  )
+}
+
+export default OnboardingTutorial
