b87e7f91a7ddc4ff199c4ee43d3b776e434965b0
mschild91 <michael.schild@mail.de>
2023-10-25 09:47:10 +0200

    fixed logout imports

--- src/components/pages/Account/Button/LogoutButton/LogoutButton.jsx
+++ src/components/pages/Account/Button/LogoutButton/LogoutButton.jsx
@@ -2,8 +2,15 @@ import React from "react"
 import style from "./LogoutButton.module.scss"
 import LogoutIcon from "../../../../../assets/images/logout.png"
 import Arrow from "../../../../../assets/images/arrow-right.svg"
+import { useContext } from "react"
+import { UserContext } from "../../../../../contexts/userContext.jsx"
+import { useNavigate } from "react-router-dom"
 
 function LogoutButton() {
+  const { setUserData } = useContext(UserContext)
+
+  const navigator = useNavigate()
+
   const logout = async () => {
     const response = await fetch(
       import.meta.env.VITE_SERVER + "api/auth/logout",
