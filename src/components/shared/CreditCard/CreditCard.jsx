e6f3b7b0dda025b5a69534397194393ee4ce616c
Your Name <ali3ajob@gmail.com>
2023-10-25 14:38:29 +0200

    card info

--- src/components/shared/CreditCard/CreditCard.jsx
+++ src/components/shared/CreditCard/CreditCard.jsx
@@ -1,10 +1,30 @@
-import React from "react"
+import React, { useContext, useEffect, useState } from "react"
 import creditCard from "../../../assets/images/Card.png"
 import style from "./CreditCard.module.scss"
+import { UserContext } from "../../../contexts/userContext"
 
 function CreditCard() {
+  const { userData } = useContext(UserContext)
+  const [cardNum, setCardNum] = useState("")
+  const [cardExp, setCardExp] = useState("")
+
+  useEffect(() => {
+    let cardNumber = ''
+    let cardExpiration = ''
+
+    if (userData.userAllCards) {
+      cardNumber = userData.userAllCards[0].card_number.slice(15, 19)
+      cardExpiration = userData.userAllCards[0].expiration_date
+    }
+    
+    setCardNum(cardNumber)
+    setCardExp(cardExpiration)
+  }, [userData])
+
   return (
     <div className={style.credit_card}>
+      <p>{cardNum}</p>
+      <p>{cardExp}</p>
       <img
         src={creditCard}
         alt="Credit Card"
